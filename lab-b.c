05a3b517d014667e8d94bf39bdc77fb19fad0f54
xuan tang <txzzz@mail.ustc.edu.cn>
2024-12-17 18:26:05 +0800

    labb debug

--- lab-b.c
+++ lab-b.c
@@ -2,40 +2,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int my_Alltoall(void *sendbuf, int sendcount, MPI_Datatype sendtype, void* recvbuf, int recvcount, MPI_Datatype recvtype, MPI_Comm comm){
+
+int my_Alltoall(void *sendbuf, int sendcount, MPI_Datatype sendtype, 
+                void *recvbuf, int recvcount, MPI_Datatype recvtype, 
+                MPI_Comm comm) {
     int world_size, world_rank;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    int sendtype_size = sizeof(sendtype);
-    for(int i = 0; i < world_size; ++ i){
-        MPI_Send(sendbuf + i * sendtype_size * sendcount, sendcount, sendtype, i, 0, comm);
+
+    int sendtype_size, recvtype_size;
+    MPI_Type_size(sendtype, &sendtype_size); 
+    MPI_Type_size(recvtype, &recvtype_size);  
+
+    for (int i = 0; i < world_size; ++i) {
+        MPI_Send(sendbuf + i * sendtype_size * sendcount, sendcount, sendtype, 
+                 i, 0, comm);
     }
+
     MPI_Barrier(comm);
-    int recvtype_size = sizeof(recvtype);
-    for(int i = 0; i < world_size; ++ i){
-        MPI_Recv(recvbuf + i * recvtype_size * recvcount, recvcount, recvtype, i, 0, comm, MPI_STATUS_IGNORE);
+    for (int i = 0; i < world_size; ++i) {
+        MPI_Recv(recvbuf + i * recvtype_size * recvcount, recvcount, recvtype, 
+                 i, 0, comm, MPI_STATUS_IGNORE);
     }
+
+    return 0;
 }
+
 int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);
 
     int world_size, world_rank;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
     int *sendbuf = (int*)malloc(sizeof(int) * world_size);
     int *recvbuf = (int*)malloc(sizeof(int) * world_size);
-    for(int i = 0; i < world_size; ++ i){
+
+    for (int i = 0; i < world_size; ++i) {
         sendbuf[i] = i;
         recvbuf[i] = -1;
     }
+
     my_Alltoall(sendbuf, 1, MPI_INT, recvbuf, 1, MPI_INT, MPI_COMM_WORLD);
+
     printf("cur_rank: %d, recvdata: ", world_rank);
-    for(int i = 0; i < world_size; ++ i){
+    for (int i = 0; i < world_size; ++i) {
         printf("%d ", recvbuf[i]);
     }
     printf("\n");
+
     free(sendbuf);
     free(recvbuf);
+
     MPI_Finalize();
     return 0;
 }
