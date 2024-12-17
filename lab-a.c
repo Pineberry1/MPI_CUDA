df8a1312c76547b6cd5e6b7e0ed9a162e6f3a9d7
xuan tang <txzzz@mail.ustc.edu.cn>
2024-12-17 16:52:26 +0800

    debug

--- lab-a.c
+++ lab-a.c
@@ -11,7 +11,7 @@ int MyBcastMPI(void* data, int count, MPI_Datatype datatype, int root, MPI_Comm 
 int main(int argc, char *argv[])
 {
     int id_procs, num_procs;
-    char seq[16];
+    char buf[16];
     int root = 0;
     MPI_Group world_group, new_group;
     MPI_Init(&argc, &argv);
@@ -19,9 +19,8 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &id_procs);
     if (id_procs == root)
     {
-        strcpy(seq, "hello,MPI!");
+        strcpy(buf, "hello,MPI!");
     }
-    MPI_Barrier(MPI_COMM_WORLD);
     //1.1按节点分组
     MPI_Comm split_comm_world;
     MPI_Status status;
@@ -32,36 +31,26 @@ int main(int argc, char *argv[])
     MPI_Comm_split(MPI_COMM_WORLD, id_procs % 4, id_procs, &split_comm_world);
     MPI_Comm_rank(split_comm_world, &rank);
     MPI_Comm_size(split_comm_world, &size);
-    printf("MPI Comm rank %d, node id %d, size %d. the new msg is %s\n", id_procs, rank);
-    //create new group H
-    MPI_Comm h_comm_world;
-    MPI_Comm_group(MPI_COMM_WORLD, &world_group);
-    int grpsize = num_procs / 2;
-    int zerolist[] = {0, 1, 2, 3};
-    int zerocnt = 0;
-
-    MPI_Group_incl(world_group, grpsize, zerolist, &new_group);
-    MPI_Comm_create(MPI_COMM_WORLD, new_group, &h_comm_world);
-    // message from root to 0 proc of MPI_COMM_WORLD
-    if (id_procs == root)
-    {
-        MPI_Send(&seq, 16, MPI_CHAR, 0, 1, MPI_COMM_WORLD);
+    printf("MPI Comm rank %d, node id %d\n", id_procs, rank);
+    MPI_Barrier(MPI_COMM_WORLD);
+    //1.2root发送到每个节点的0号进程
+    if(id_procs == root){
+        char message[16] = "signal";
+        for(int i = 0; i < num_procs; ++ i){
+            if(rank == 0){
+                MPI_SEND(&message, 16, MPI_CHAR, i, 0, MPI_COMM_WORLD);
+            }
+        }
     }
-    else if (id_procs == 0)
-    {
-        MPI_Recv(&seq, 16, MPI_CHAR, root, 1, MPI_COMM_WORLD, &status);
+    //0号进程接收并广播
+    MPI_Barrier(MPI_COMM_WORLD);
+    if(rank == 0){
+        MPI_RECV(&buf, 16, MPI_CHAR, root, 0, MPI_COMM_WORLD);
     }
-    MPI_Barrier(MPI_COMM_WORLD);
-    // Broadcast within the group H
-    if(h_comm_world != MPI_COMM_NULL)
-        MPI_Bcast(&seq, 16, MPI_CHAR, 0, h_comm_world);
-    MPI_Barrier(MPI_COMM_WORLD);
-    //Broadcasr within the group N
-
-    MPI_Bcast(&seq, 16, MPI_CHAR, 0, split_comm_world);
+    MPI_Bcast(&buf, 16, MPI_CHAR, 0, split_comm_world);
     MPI_Barrier(MPI_COMM_WORLD);
 
-    printf("MPI Comm rank %d, original id %d, size %d. the new msg is %s\n", rank, id_procs, size, seq);
+    printf("MPI Comm rank %d, node id %d, buf: %s\n", id_procs, rank, buf);
     MPI_Finalize();
     return 0;
 }
