413a0baee1d05b1c9edc906bf62f3cedf49f0eda
xuan tang <txzzz@mail.ustc.edu.cn>
2024-12-18 17:48:28 +0800

    debug c

--- lab-c-2.c
+++ lab-c-2.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[]) {
 
     // 二叉树全和
     int step, recv;
-    for (step = 1; step < N; step *= 2) {
+    for (step = 1; step < N; step <<= 1) {
         if(world_rank % (step << 1) == 0){
             int partner = world_rank + step;
             if(partner < N){
@@ -27,7 +27,18 @@ int main(int argc, char *argv[]) {
             MPI_Send(&local_sum, 1, MPI_INT, world_rank - step, step, MPI_COMM_WORLD);
         }
     }
-    
+    for(;step > 1; step >>= 1){
+        if(world_rank % step == 0){
+            int partner = world_rank + (step >> 1);
+            if(partner < N){
+                MPI_Send(&local_sum, 1, MPI_INT, partner, step, MPI_COMM_WORLD);
+            }
+        }
+        if(world_rank - (step >> 1) >= 0 && (world_rank - (step >> 1)) % (step) == 0){
+            MPI_Recv(&recv, 1, MPI_INT, world_rank - (step >> 1), step, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            local_sum = recv;
+        }
+    }
     printf("world_rank %d, Global sum: %d\n", world_rank, local_sum);
 
     MPI_Finalize();
